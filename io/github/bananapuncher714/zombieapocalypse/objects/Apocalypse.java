5d06918cad1c0548a313627a12051a6e6580f2e3
BananaPuncher714 <danielgu714@gmail.com>
2018-05-26 17:50:56 -0400

    Added spawn and reward amounts

--- io/github/bananapuncher714/zombieapocalypse/objects/Apocalypse.java
+++ io/github/bananapuncher714/zombieapocalypse/objects/Apocalypse.java
@@ -37,7 +37,9 @@ public class Apocalypse {
 	EndState endOnFinish = EndState.END_TIME;
 	int playersRequired = 1;
 	Set< UUID > participants = new HashSet< UUID >(); // Who's playing; Only useful when apocalypse is running
+	int spawnCount = 1;
 	Map< SpawnSet, Integer > spawns = new HashMap< SpawnSet, Integer >(); // The different spawnSets to spawn mobs
+	int rewardCount = 1;
 	Map< RewardSet, Integer > rewards = new HashMap< RewardSet, Integer >(); // The rewards for winning, one will be chosen at random
 
 	Map< UUID, Boolean > monsters = new HashMap< UUID, Boolean >(); // Keep track of mobs
@@ -46,7 +48,7 @@ public class Apocalypse {
 		this.world = world;
 		this.id = id.replaceAll( "\\s+", "" );
 	}
-	
+
 	public Apocalypse( String id, FileConfiguration config ) {
 		this.id = id;
 		world = Bukkit.getWorld( config.getString( "world" ) );
@@ -59,6 +61,8 @@ public class Apocalypse {
 		killPercentRequired = config.getDouble( "kill-percent-required" );
 		playersRequired = config.getInt( "players-required" );
 		endOnFinish = EndState.valueOf( config.getString( "end-state" ).toUpperCase() );
+		spawnCount = Math.max( 1, config.getInt( "spawn-amount" ) );
+		rewardCount = Math.max( 1, config.getInt( "reward-amount" ) );
 		for ( String string : config.getStringList( "rewards" ) ) {
 			String[] data = string.split( ":" );
 			RewardSet set = ApocalypseManager.getInstance().getRewardSet( data[ 0 ] );
@@ -114,12 +118,12 @@ public class Apocalypse {
 	public EndState getEndState() {
 		return endOnFinish;
 	}
-	
+
 	public Apocalypse setEndState( EndState state ) {
 		endOnFinish = state;
 		return this;
 	}
-	
+
 	public double getChance() {
 		return possibility;
 	}
@@ -150,12 +154,12 @@ public class Apocalypse {
 	public boolean isParticipant( Player player ) {
 		return participants.contains( player.getUniqueId() );
 	}
-	
+
 	public Apocalypse addSpawnSet( SpawnSet set, int weight ) {
 		spawns.put( set, weight );
 		return this;
 	}
-	
+
 	public boolean removeSpawnSet( SpawnSet set ) {
 		boolean contains = spawns.containsKey( set );
 		if ( contains ) {
@@ -168,7 +172,7 @@ public class Apocalypse {
 		rewards.put( set, weight );
 		return this;
 	}
-	
+
 	public boolean removeRewardSet( RewardSet set ) {
 		boolean contains = rewards.containsKey( set );
 		if ( contains ) {
@@ -176,7 +180,7 @@ public class Apocalypse {
 		}
 		return contains;
 	}
-	
+
 	public void start() {
 		participants.clear();
 		for ( Player player : world.getPlayers() ) {
@@ -188,14 +192,20 @@ public class Apocalypse {
 		}
 		isRunning = true;
 		ZombieApocalypse.getConsoleLogger().info( "Starting apocalypse '" + id + "'" );
-		
-		SpawnSet set = Util.getRandom( spawns );
+
 		monsters.clear();
-		for ( UUID uuid : participants ) {
-			Player player = Bukkit.getPlayer( uuid );
-			player.sendMessage( ZombieApocalypse.parse( "notifications.started-apocalypse", player ) );
-			player.playSound( player.getLocation(), Sound.AMBIENT_CAVE, 1, 1 );
-			spawnMobs( set, player );
+		boolean firstPass = true;
+		for ( int i = 0; i < spawnCount; i++ ) {
+			SpawnSet set = Util.getRandom( spawns );
+			for ( UUID uuid : participants ) {
+				Player player = Bukkit.getPlayer( uuid );
+				if ( firstPass ) {
+					player.sendMessage( ZombieApocalypse.parse( "notifications.started-apocalypse", player ) );
+					player.playSound( player.getLocation(), Sound.AMBIENT_CAVE, 1, 1 );
+				}
+				spawnMobs( set, player );
+			}
+			firstPass = false;
 		}
 	}
 
@@ -239,7 +249,7 @@ public class Apocalypse {
 		if ( cancel ) {
 			return;
 		}
-		
+
 		if ( participants.size() < playersRequired ) {
 			for ( UUID uuid : participants ) {
 				Player player = Bukkit.getPlayer( uuid );
@@ -250,13 +260,18 @@ public class Apocalypse {
 
 		// See if the kill percent is reached
 		if ( percentCleared >= killPercentRequired ) {
-			RewardSet reward = Util.getRandom( rewards );
-
-			for ( UUID uuid : participants ) {
-				Player player = Bukkit.getPlayer( uuid );
-				player.sendMessage( ZombieApocalypse.parse( "notifications.win-stuff", player ) );
-				player.playSound( player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1 );
-				giveRewards( reward, player, percentCleared );
+			boolean firstPass = true;
+			for ( int i = 0; i < rewardCount; i++ ) {
+				RewardSet reward = Util.getRandom( rewards );
+				for ( UUID uuid : participants ) {
+					Player player = Bukkit.getPlayer( uuid );
+					if ( firstPass ) {
+						player.sendMessage( ZombieApocalypse.parse( "notifications.win-stuff", player ) );
+						player.playSound( player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1 );
+					}
+					giveRewards( reward, player, percentCleared );
+				}
+				firstPass = false;
 			}
 		} else {
 			for ( UUID uuid : participants ) {
@@ -291,7 +306,7 @@ public class Apocalypse {
 	public Set< UUID > getMobs() {
 		return monsters.keySet();
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning;
 	}
@@ -299,7 +314,7 @@ public class Apocalypse {
 	public double getPercentCleared() {
 		return getAmountCleared() / ( double ) monsters.size();
 	}
-	
+
 	public int getAmountCleared() {
 		int i = 0;
 		for ( boolean value : monsters.values() ) {
@@ -309,11 +324,11 @@ public class Apocalypse {
 		}
 		return i;
 	}
-	
+
 	public int getAmountTotal() {
 		return monsters.size();
 	}
-	
+
 	// Run things depending on events
 	public void onPlayerDeathEvent( PlayerDeathEvent event ) {
 		participants.remove( event.getEntity().getUniqueId() );
@@ -353,14 +368,14 @@ public class Apocalypse {
 			update();
 		}
 	}
-	
+
 	public void onEntityExplodeEvent( EntityExplodeEvent event ) {
 		Entity entity = event.getEntity();
 		if ( monsters.containsKey( entity.getUniqueId() ) ) {
 			monsters.remove( entity.getUniqueId() );
 		}
 	}
-	
+
 	public enum EndState {
 		KILL_ALL, KILL_REQUIRED, END_TIME;
 	}
